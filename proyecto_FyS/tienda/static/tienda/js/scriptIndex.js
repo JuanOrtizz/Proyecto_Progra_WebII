008f22332b4f224ee924374a493e9d5c70dca541
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-05-10 14:54:39 -0300

    fix: Correccion de error del modal

--- proyecto_FyS/tienda/static/tienda/js/scriptIndex.js
+++ proyecto_FyS/tienda/static/tienda/js/scriptIndex.js
@@ -1,9 +1,25 @@
-/*Uso unicamente esta funcion lambda de JS ya que al cambiar de boton del modal por el del formulario,
-necesito implementarla para que funcione*/
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   var modalElement = document.getElementById('exampleModal')
+  var botonEnviarFormulario = document.getElementById('boton_enviar_form')
+
   if (modalElement) {
     var myModal = new bootstrap.Modal(modalElement)
+
+    // muestro el modal
     myModal.show()
+
+    //cuando el modal se cierra, mueve el foco al boton que lo abrio (boton del formulario)
+    modalElement.addEventListener('hidden.bs.modal', () => {
+      botonEnviarFormulario.focus()
+
+      // borra estilos aplicados por el modal (como el fondo oscuro)
+      myModal.dispose()
+
+      // vuelvo el estilo overflow para permitir el scroll
+      document.body.style.overflow = ''
+
+      // elimino el margen que le pone el modal al body
+      document.body.style.paddingRight = '0px'
+    })
   }
 })
