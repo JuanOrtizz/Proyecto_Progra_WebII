49a94d19975090396319cca8664d2908275f7897
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-05-10 14:31:24 -0300

    feat: Agrego templates y static de la app y modificaciones de archivos del proyecto

--- proyecto_FyS/tienda/urls.py
+++ proyecto_FyS/tienda/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.pagina_tienda_fys, name='index'),
+    path('sucursales/', views.sucursales, name='sucursales'), #agregue la urls para la pagina sucursales
 ]
 
