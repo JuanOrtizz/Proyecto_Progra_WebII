1a15d1096ae7d3ef0ec98d5e17ab25716379af8a
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-07-11 02:24:57 -0300

    feat: Integracion de confirmacion en Updates en Productos y Consultas

--- tienda/static/tienda/js/modificarConsulta.js
+++ tienda/static/tienda/js/modificarConsulta.js
@@ -9,18 +9,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     // Evento para evitar que se mande el form
     if(formularioModificarConsulta){
-            formularioModificarConsulta.addEventListener('submit', (e) =>{
-                // evita recargar la pagina al mandar el formulario
-                e.preventDefault()
-                // captura los datos del formulario
-                let formData = new FormData(formularioModificarConsulta)
-                // valida los campos
-                if(validarFormulario(formData)){
-                    enviarFormulario(formData, csrfToken)
-                }
-            })
+        formularioModificarConsulta.addEventListener('submit', (e) =>{
+            // evita recargar la pagina al mandar el formulario
+            e.preventDefault()
+            // captura los datos del formulario
+            let formData = new FormData(formularioModificarConsulta)
+            // valida los campos
+            if(validarFormulario(formData)){
+                Swal.fire({ // Muestra alerta
+                    title: "¿Estás seguro que querés modificar esta consulta?",
+                    text: "No podrás revertir esto",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Sí Modificar",
+                    didOpen: () =>{ // agrego esto ya que sino recalcula con esta clase y sube el footer para evitar scroll
+                        document.body.classList.remove('swal2-height-auto')
+                        document.body.style.overflow = 'auto'
+                        document.body.style.paddingRight = '0'
+                    }
+                }).then((result) => {
+                    if (result.isConfirmed) { // si confirma envia el formulario y modifica la consulta
+                        enviarFormulario(formData, csrfToken)
+                    }
+                })
+            }
+        })
     }
-
 })
 
 // funcion async para utilizar await y manejar asincronia
@@ -45,11 +61,11 @@ async function enviarFormulario(formData, csrfToken){
                 generarAlertError(data.errors)
             }
             else{
-                generarAlertError("Ocurrió un error al modificar la consulta. Intenta nuevamente")
+                generarAlertError("Ocurrió un error al modificar la consulta. Intentá nuevamente.")
             }
         }
     }catch(error){
         console.error("Error en la modificación: ", error)
-        generarAlertError("Error al modificar la consulta. Intenta mas tarde")
+        generarAlertError("Error al modificar la consulta. Intentá más tarde.")
     }
 }
