3f0d1cf51c0af1cc22a7ef12bc87750dfaea216e
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-07-11 02:24:57 -0300

    feat: Integracion de confirmacion en Updates en Productos y Consultas

--- tienda/static/tienda/js/modificarProducto.js
+++ tienda/static/tienda/js/modificarProducto.js
@@ -16,7 +16,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 let formData = new FormData(formularioModificarProducto)
                 // valida los campos
                 if(validarFormulario(formData)){
-                    enviarFormulario(formData, csrfToken)
+                    Swal.fire({ // Muestra alerta
+                        title: "¿Estás seguro que querés modificar este producto?",
+                        text: "No podrás revertir esto",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Sí Modificar",
+                        didOpen: () =>{ // agrego esto ya que sino recalcula con esta clase y sube el footer para evitar scroll
+                            document.body.classList.remove('swal2-height-auto')
+                            document.body.style.overflow = 'auto'
+                            document.body.style.paddingRight = '0'
+                        }
+                    }).then((result) => {
+                        if (result.isConfirmed) { // si confirma envia el formulario y modifica el producto
+                            enviarFormulario(formData, csrfToken)
+                        }
+                    })
                 }
             })
     }
@@ -36,7 +53,6 @@ async function enviarFormulario(formData, csrfToken){
             }
         })
         const data = await response.json()
-        console.log("hola")
         if(data.success){
             // guarda en el sessionStorage el mensaje de exito para mostrar en productos.html
             sessionStorage.setItem('mensajeExito', data.message)
@@ -48,11 +64,11 @@ async function enviarFormulario(formData, csrfToken){
                 generarAlertError(data.errors)
             }
             else{
-                generarAlertError("Ocurrió un error al modificar el producto. Intenta nuevamente")
+                generarAlertError("Ocurrió un error al modificar el producto. Intenta nuevamente.")
             }
         }
     }catch(error){
         console.error("Error en la modificación: ", error)
-        generarAlertError("Error al modificar el producto. Intenta mas tarde")
+        generarAlertError("Error al modificar el producto. Intentá más tarde.")
     }
 }
