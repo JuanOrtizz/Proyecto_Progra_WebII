84037b951a1f9f19fa0933ab81fa6b73eacfa116
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-07-14 23:47:58 -0300

    refactor: Correcciones en codigo css de la app 'Tienda'

--- tienda/static/tienda/js/formRegistrarProducto.js
+++ tienda/static/tienda/js/formRegistrarProducto.js
@@ -1,5 +1,6 @@
 import {validarFormulario} from './validacionesFormProductos.js'
 import {generarAlertExito, generarAlertError} from './alertas.js'
+import {textoErrorInput, eliminarErrorInput} from './erroresInputs.js'
 
 document.addEventListener('DOMContentLoaded', ()=>{
     // capturo el select para aplicarle un estilo para mejorar UX
@@ -14,23 +15,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     // Evento para evitar que se mande el form
     formularioProducto.addEventListener('submit', (e) =>{
-            // evita recargar la pagina al mandar el formulario
-            e.preventDefault()
-            // captura los datos del formulario
-            const formData = new FormData(formularioProducto)
-            // valida los campos
-            if(validarFormulario(formData)){
-                enviarFormulario(formData, csrfToken, formularioProducto)
-            }
-        })
+        // evita recargar la pagina al mandar el formulario
+        e.preventDefault()
+        // captura los datos del formulario
+        const formData = new FormData(formularioProducto)
+        // valida los campos
+        if(validarFormulario(formData)){
+            enviarFormulario(formData, csrfToken, formularioProducto)
+        }
+    })
 })
 
 // funcion async para utilizar await y manejar asincronia
 async function enviarFormulario(formData, csrfToken, formularioProducto){
+    const overlay = document.getElementById("pantalla_carga") // Obtiene el overlay (pantalla de carga)
+    overlay.style.display = "flex" // Muestra el overlay (pantalla de carga)
     // hago fetch del formulario
     try
     {
-        const response = await fetch("",{
+        const response = await fetch("/dashboard/productos/registrar-producto/",{
             method:"POST",
             body: formData,
             headers:{
@@ -40,20 +43,29 @@ async function enviarFormulario(formData, csrfToken, formularioProducto){
 
         const data = await response.json()
         if(data.success){
-            generarAlertExito(data.message)
-
             // vacio el formulario
             formularioProducto.reset()
-
             // elimino la clase de forma manual del select para que no parezca que sigue seleccionado al vaciar el formulario
             document.querySelectorAll('.select_seleccionado, .select_no_seleccionado').forEach(el => el.classList.remove('select_seleccionado', 'select_no_seleccionado'))
-
+            // guarda en el sessionStorage el mensaje de exito para mostrar en productos.html
+            sessionStorage.setItem('mensajeExito', data.message)
+            window.location.href = "/dashboard/productos/" //redirige a productos.html
         }else{
-            generarAlertError(formatearErrores(data.errors))
+            const errores = data.errors //capturo los errores
+            for (let campo in errores) {
+                const mensaje = errores[campo][0]
+                const input = document.getElementById(campo)
+                if(input) {
+                    textoErrorInput(input, mensaje)
+                }
+            }
         }
     }catch(error){
         console.error("Error en el Registro: ", error)
         generarAlertError("Ocurrió un error inesperado. Intentá más tarde.")
+    }
+    finally {
+        overlay.style.display = "none" // oculta el overlay (pantalla de carga)
     }
 }
 
@@ -66,14 +78,4 @@ function asignarClaseSelect(select, valor){
         select.classList.add("select_no_seleccionado")
         select.classList.remove("select_seleccionado")
     }
-}
-
-function formatearErrores(errores) {
-    let mensajes = []
-    for (const campo in errores) {
-        if (errores.hasOwnProperty(campo)) {
-          mensajes.push(`${campo}: ${errores[campo].join(', ')}`)
-        }
-    }
-    return mensajes.join('\n')
 }
