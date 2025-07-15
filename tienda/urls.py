74f316fdc1760b2a5223ad87a7cec723a6d71a92
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-07-14 23:52:30 -0300

    feat: Integracion de nuevas funcionalidades (Recuperar cuenta, cambiar contraseña y cargar archivos media desde servidor en DEBUG = FALSE) y correcciones de codigo PY en vistas

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
-from .views import ConsultasViewSet, ProductosViewSet, ObtenerFraseAPIView
+from .views import ConsultasViewSet, ProductosViewSet, ObtenerFraseAPIView, CustomPasswordResetView, \
+    CustomPasswordResetConfirmView, CustomPasswordChangeView, MediaServeView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -16,22 +17,23 @@ urlpatterns = [
     path('colecciones/', views.colecciones, name="colecciones"), #path para el formulario de contacto
     path('locales/', views.locales, name='locales'), #path para los locales
     path('contacto/', views.contacto, name="contacto"), #path para el formulario de contacto
-    path('coleccion/<str:tipo>/', views.coleccion_filtrada, name='coleccion_filtrada'), #path para las colecciones filtradas (Por ejemplo buzos)
+    path('coleccion/<str:tipo>/', views.coleccion_filtrada, name='coleccion-filtrada'), #path para las colecciones filtradas (Por ejemplo buzos)
     path('login/', auth_views.LoginView.as_view(template_name='tienda/login.html'), name='login'),  # path para el login
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # path para el logout
     path('registro/', views.registro, name="registro"), #path para el registro
-    path('validar_registro/', views.validar_registro, name="validar_registro"), #path para validar el registro
-    path('recuperar_contraseña/', views.recuperar_contraseña, name="recuperar_contraseña"),# path para recuperar contraseña
-    path('cambiar_contraseña/', views.cambiar_contrasenia, name="cambiar_contraseña"), #path para cambiar la contraseña
+    path('validar-registro/', views.validar_registro, name="validar-registro"), #path para validar el registro
+    path('recuperar-cuenta/', CustomPasswordResetView.as_view(), name='recuperar-cuenta'), #path para recuperar la cuenta desde "Olvidé mi contraseña"
+    path('cambiar-contrasenia/', CustomPasswordChangeView.as_view(), name='cambiar-contrasenia'), #path para cambiar la contraseña si esta logeado
+    path('recuperar-contrasenia/confirmar/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='recuperar-contrasenia-confirmar'), # path para cambiar la constraseña si no esta logeado (Olvide mi contraseña), se obtiene por mail
     path('dashboard/', views.dashboard, name='dashboard'),  # path para el dashboard
     path('dashboard/consultas/', views.consultas, name='consultas'),#path para las consultas desde el dashboard
     path('dashboard/productos/', views.productos, name='productos'), #path para los productos desde el dashboard
-    path('dashboard/productos/agregarProducto/', views.registrar_producto, name='agregar_producto'), #path para agregar un producto
-    path('dashboard/consultas/modificarConsulta/<int:consulta_id>/', views.modificar_consulta, name='modificar_consulta'),# path para modificar consultas
-    path('dashboard/consultas/eliminarConsulta/<int:consulta_id>/', views.eliminar_consulta, name='eliminar_consulta'),# path para eliminar consultas
-    path('dashboard/productos/modificarProducto/<int:producto_id>/', views.modificar_producto, name='modificar_producto'),# path para modificar productos
-    path('dashboard/productos/eliminarProducto/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),# path para eliminar productos
-    path('obtener_frase/', ObtenerFraseAPIView.as_view(), name='obtener_frase'),  # agrego esta ruta para la API externa
+    path('dashboard/consultas/modificar-consulta/<int:consulta_id>/', views.modificar_consulta, name='modificar-consulta'),# path para modificar consultas
+    path('dashboard/consultas/eliminar-consulta/<int:consulta_id>/', views.eliminar_consulta, name='eliminar-consulta'),# path para eliminar consultas
+    path('dashboard/productos/registrar-producto/', views.registrar_producto, name='registrar-producto'),# path para agregar un producto
+    path('dashboard/productos/modificar-producto/<int:producto_id>/', views.modificar_producto, name='modificar-producto'),# path para modificar productos
+    path('dashboard/productos/eliminar-producto/<int:producto_id>/', views.eliminar_producto, name='eliminar-producto'),# path para eliminar productos
+    path('obtener-frase/', ObtenerFraseAPIView.as_view(), name='obtener-frase'),  # agrego esta ruta para la API externa
     path('api/', include(router.urls)), #path para exponer los endpoints con el router
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    re_path(r'^media/(?P<path>.*)$', MediaServeView.as_view(), name='media'), #re_path para servir archivos media en produccion con DEBUG =FALSE
+]
